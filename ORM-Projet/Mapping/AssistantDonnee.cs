f43733f0c4bad80aed830769e1c89792f33cb54d
Sébastien OUALLET <sebastien.ouallet@gmail.com>
2017-12-10 20:30:40 +0100

    J'ai remis les fichiers mapping de mathieu et edwin dans le projet et j'ai modifier le namespace des class de mapping

---
+++ ORM-Projet/Mapping/AssistantDonnee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMProjet.Mapping
+{
+    //Obtient la liste des noms de colonnes de NomDonnee:
+    public static class AssistantDonnee
+    {
+    
+        public static List<string> GetDataNames(Type type, string propertyName)
+        {
+            var proprite = type.GetProperty(propertyName).GetCustomAttributes(false).Where(x => x.GetType().Name == "NomDonnee").FirstOrDefault();
+            if (proprite != null)
+            {
+                return ((NomDonnee)proprite).ValueNames;
+            }
+            return new List<string>();
+        }
+    }
+}
---
+++ ORM-Projet/Mapping/NomDonnee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMProjet.Mapping
+{
+    //Mapping des noms de colones sur les objets correspondants 
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NomDonnee : Attribute
+    {
+        protected List<string> _valueNames { get; set; }
+
+        public List<string> ValueNames
+        {
+            get
+            {
+                return _valueNames;
+            }
+            set
+            {
+                _valueNames = value;
+            }
+        }
+
+        public NomDonnee()
+        {
+            _valueNames = new List<string>();
+        }
+
+        public NomDonnee(params string[] valueNames)
+        {
+            _valueNames = valueNames.ToList();
+        }
+    }
+}
